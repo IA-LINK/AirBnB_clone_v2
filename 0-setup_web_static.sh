e4a06aa27ee7e83a89da562059430c5c06bc71c1
IA-LINK <jyc.kom@gmail.com>
2024-07-30 23:32:46 -0700

    task refixed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,21 +1,27 @@
 #!/usr/bin/env bash
-# sets up the web servers for the deployment of web_static
+# a Bash script that sets up your web servers for the deployment of web_static
 
-sudo apt-get -y update
+# Install Nginx if not already installed
+sudo apt-get update
 sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
 
+# Create necessary folders
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 
+# Create fake HTML file
+echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
+
+# Create symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
-sudo service nginx start
+
+# Set ownership
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update Nginx configuration
+sudo sed -i 's|^.*server_name.*$|&\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n|' /etc/nginx/sites-available/default
+
+
+# Restart Nginx
+sudo service nginx restart
+
+exit 0
